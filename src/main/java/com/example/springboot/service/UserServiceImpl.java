342bd100024f654cbadb402c7f64591b333de354
Us3rL0sT <105300681+Us3rL0sT@users.noreply.github.com>
2023-01-24 22:01:08 +0500

    3

--- src/main/java/com/example/springboot/service/UserServiceImpl.java
+++ src/main/java/com/example/springboot/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.springboot.model.User;
 import com.example.springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,19 +20,27 @@ public class UserServiceImpl implements UserService {
     }
 
 
+    @Transactional(readOnly = true)
+    @Override
     public User getUserById(int id) {
         return userRepository.getOne(id);
     }
 
+    @Transactional(readOnly = true)
+    @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    @Transactional
+    @Override
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
 
+    @Transactional
+    @Override
     public void removeUserById(int id) {
         userRepository.deleteById(id);
     }
